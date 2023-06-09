6eb3adc7c1f999bed34e14d78f4069f2d39199cb
Dinesh Gupta <Dineshg0009@gmail.com>
2023-06-09 14:35:16 +0530

    Added Multiple file upload

--- src/main/java/com/RestAPI/Controller/ImageController.java
+++ src/main/java/com/RestAPI/Controller/ImageController.java
@@ -1,6 +1,7 @@
 package com.RestAPI.Controller;
 
 import com.RestAPI.Service.EmployeeImageService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,7 +11,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
 public class ImageController {
 
     @Autowired
@@ -22,6 +27,7 @@ public class ImageController {
         imageService.uploadImage(file);
     }
 
+    @GetMapping("/download/{filename}")
     public ResponseEntity<byte[]> downloadImage(@PathVariable String fileName){
         byte[] image=imageService.downloadImage(fileName);
         return  ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png")).body(image);
